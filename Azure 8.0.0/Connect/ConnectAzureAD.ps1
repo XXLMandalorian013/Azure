fe30721cc3cef74d093fd8f9834f8ad2c4b12af5
Drew McClellan <d.mcclellan013@gmail.com>
2022-06-30 12:21:27 -0400

    Delete ConnectAzureAD.ps1

--- Azure 8.0.0/Connect/ConnectAzureAD.ps1
+++
@@ -1,5 +0,0 @@
-#Connect to Azure A/D w/ PS. A browser will open and prompt for Credential.
-
-    #https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-8.0.0#sign-in
-
-        Connect-AzAccount
